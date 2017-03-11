4ab062a123916d915aaf47052f993ddb55a944b8
Alexander <alexander.lock7@gmail.com>
2017-03-11 04:02:57 +0000

    click listener works for numbers and operations

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -1,5 +1,19 @@
 'use strict'
 
 $(document).ready(() => {
+    let number = "";
+    let newnumber = "";
+    let operator = "";
+    let display = $("#display");
+    display.val("0");
 
+  // Numbers added to display and equation
+   $("[name=number]").click(() => {
+          display.val($(this).val());
+    });
+
+  // Operations added to display and equation
+  $("[name=operator]").click(() => {
+          display.val($(this).val());
+    });
 });
