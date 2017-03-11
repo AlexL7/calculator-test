757b7c08605542e5ab026c0bd3ffde9720bfe998
Alexander <alexander.lock7@gmail.com>
2017-03-11 14:32:56 +0000

    clear and clearall works

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -1,18 +1,33 @@
 'use strict'
 
 $(document).ready(function() {
-    let number = "";
-    let newnumber = "";
-    let operator = "";
+    let equation = "";
+    let answer = "";
     let display = $("#display");
     display.val("0");
 
   // Numbers added to display and equation
    $("[name=number]").click(function(){
-         display.val($(this).val())
+         equation += ($(this).val());
+         display.val(equation);
     });
   // Operations added to display and equation
-  $("[name=operator]").click(function(){
-          display.val($(this).val());
+   $("[name=operator]").not("#clear,#clearAll").click(function(){
+         equation += ($(this).val());
+         display.val(equation);
+    });
+
+   $("#clear").click(function(){
+         equation = equation.slice(0,-1);
+         display.val(equation);
+    });
+
+   $("#clearAll").click(function(){
+         equation = "0";
+         display.val(equation);
+    });
+
+   $("#equals").click(function(){
+
     });
 });
