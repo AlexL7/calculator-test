e16f344f873d4e52b82ca300386c221e99c218c2
Alexander <alexander.lock7@gmail.com>
2017-03-13 18:19:31 +0000

    calculate works, paraentheses works' '

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -1,18 +1,24 @@
 'use strict'
 
 $(document).ready(function() {
-    let equation = "";
-    let answer = "";
+    let equation = "0";
+    let memory = 0;
     let display = $("#display");
     display.val("0");
 
   // Numbers added to display and equation
-   $("[name=number]").click(function(){
+     $("[name=number]").click(function(){
+         if(equation == "0"){
+          equation = '';
+         }
          equation += ($(this).val());
          display.val(equation);
     });
   // Operations added to display and equation
    $("[name=operator]").not("#clear,#clearAll").click(function(){
+         if(equation == "0"){
+          equation = '';
+         }
          equation += ($(this).val());
          display.val(equation);
     });
@@ -20,16 +26,27 @@ $(document).ready(function() {
    $("#clear").click(function(){
          equation = equation.slice(0,-1);
          display.val(equation);
+         if(equation.length < 1){
+          display.val("0");
+         }
     });
 
    $("#clearAll").click(function(){
-         equation = "0";
-         display.val(equation);
+         display.val("0");
+         equation = "";
     });
 
    $("#equals").click(function(){
-    display.val(calculate(parseEquation(equation)));
-    equation = '';
+    equation = calculate(paraentheses(parseEquation(equation)));
+
+    console.log(equation);
+    memory += parseFloat(equation);
+    display.val(memory.toString());
+
+    console.log(memory);
+    equation = '0';
+
+
 
 
 
@@ -42,15 +59,22 @@ $(document).ready(function() {
       let operation=[];
       let currentNum = '';
 
+
       for(var i=0, char; char = string.charAt(i); i++){
-        //check if character is an operation, if true, adds the current number an operation to array and resets current.
-        if ('*/+-'.indexOf(char) > -1) {
+        //check if character is an operation, if true, adds the current number and operation to array and resets current.
+        if ('()*/+-'.indexOf(char) > -1) {
           //checks for negative sign
           if (currentNum == '' && char == '-') {
                 currentNum = '-';
             } else {
-              operation.push(parseFloat(currentNum),char);
-              currentNum = '';
+
+              if(parseFloat(currentNum)){
+                operation.push(parseFloat(currentNum),char);
+                currentNum = '';
+              } else {
+                operation.push(char);
+              }
+
             }
         } else {
           currentNum += string.charAt(i);
@@ -60,13 +84,86 @@ $(document).ready(function() {
       if (currentNum != '') {
         operation.push(parseFloat(currentNum));
       }
+      console.log("Parse returns this " + operation);
       return operation;
    };
 
 
-   function paraentheses(equation){
-    test =
-   }
+   function paraentheses(arrEquation){
+    var newOperation = [];
+    var tempEquation = arrEquation;
+
+
+    for (var i =0; i<arrEquation.length; i++){
+
+      //checks for paraentheses then runs calculate of everything inside and pushes to newoperation
+      if(arrEquation[i] == "("){
+        let tempCalc = [];
+        let indexOfPar = [];
+        indexOfPar.push(tempEquation.indexOf("(", i),tempEquation.indexOf(")", i));
+        tempCalc = tempEquation.slice(indexOfPar[0]+1,indexOfPar[1]);
+
+
+        newOperation.push(calculate(tempCalc));
+
+        i = indexOfPar[1];
+        }
+      else{
+        newOperation.push(arrEquation[i]);
+      }
+    }
+
+
+      console.log("new Operation: " + newOperation);
+
+      return newOperation;
+
+    }
+
+
+
+
+
+   function calculate(calc) {
+  var ops = ['*', '/', '+', '-'],
+    opFunctions = [
+      function(a, b) {
+        return a * b
+      },
+      function(a, b) {
+        return a / b
+      },
+      function(a, b) {
+        return a + b
+      },
+      function(a, b) {
+        return a - b
+      }
+    ],
+    newCalc = [],
+    currentOp;
+    //peforms each operation on the incoming array.
+  for (var i = 0; i < ops.length; i++) {
+    for (var j = 0; j < calc.length; j++) {
+      if (calc[j] == ops[i]) {
+        currentOp = opFunctions[i];
+      } else if (currentOp) {
+        newCalc[newCalc.length - 1] = currentOp(newCalc[newCalc.length - 1], calc[j]);
+        currentOp = null;
+      } else {
+        newCalc.push(calc[j]);
+      }
+    }
+    calc = newCalc;
+    newCalc = [];
+  }
+  if (calc.length > 1) {
+    console.log('Error: unable to resolve calculation');
+    return calc;
+  } else {
+    return calc[0];
+  }
+}
 
 
 
